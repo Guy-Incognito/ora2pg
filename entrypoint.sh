8c71b197654b0f7c8c5459a449d4d572dca80419
Bek <jama.bek7@gmail.com>
2020-11-05 07:09:53 -0800

    Externalize oracle datasource, username and password  (#9)
    * Variables for Oracle datasource, username and password
    
    * updated README.md for oracle host/user/password
    
    Co-authored-by: jasati200 <jasati200@comcast.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+
 function check_env {
     if [ -z "$CONFIG_LOCATION" ]; then
         echo "INFO: No CONFIG_LOCATION variable provided. Using default '/config/ora2pg.conf'"
@@ -15,6 +16,32 @@ function check_env {
         echo "INFO: OUTPUT_LOCATION variable detected: '$OUTPUT_LOCATION'"
         mkdir -p ${OUTPUT_LOCATION}
     fi
+
+    ORA_HOST_FLAG=""
+    if [ -z "$ORA_HOST" ]; then
+        echo "INFO: No ORA_HOST variable provided. Using value of 'ORACLE_DSN' from '$CONFIG_LOCATION'"
+    else
+        echo "INFO: ORA_HOST_FLAG variable detected: '$ORA_HOST'"
+        ORA_HOST_FLAG=" --source $ORA_HOST "
+    fi
+
+    ORA_USER_FLAG=""
+    if [ -z "$ORA_USER" ]; then
+        echo "INFO: No ORA_USER variable provided. Using value of 'ORACLE_USER' from  '$CONFIG_LOCATION'"
+    else
+        echo "INFO: ORA_USER variable detected: '$ORA_USER'"
+        ORA_USER_FLAG=" --user $ORA_USER "
+    fi
+
+    ORA_PWD_FLAG=""
+    ORA_PWD_FLAG_MASKED=""
+    if [ -z "$ORA_PWD" ]; then
+        echo "INFO: No ORA_PWD variable provided. Using value of 'ORACLE_PWD' from  '$CONFIG_LOCATION'"
+    else
+        echo "INFO: ORA_PWD variable detected: '*******'"
+        ORA_PWD_FLAG=" --password $ORA_PWD "
+        ORA_PWD_FLAG_MASKED=" --password ******* "
+    fi
 }
 
 if [ "$1" = 'ora2pg' ]; then
@@ -22,8 +49,8 @@ if [ "$1" = 'ora2pg' ]; then
     check_env
 
     if [ -z "$2" ]; then
-        echo "INFO: no args provided. Using default: '--debug -c $CONFIG_LOCATION --basedir $OUTPUT_LOCATION'"
-        ora2pg --debug -c ${CONFIG_LOCATION} --basedir ${OUTPUT_LOCATION}
+        echo "INFO: no args provided. Using default: '--debug -c $CONFIG_LOCATION --basedir $OUTPUT_LOCATION ${ORA_HOST_FLAG} ${ORA_USER_FLAG}  ${ORA_PWD_FLAG_MASKED} ' "
+        ora2pg --debug -c ${CONFIG_LOCATION} --basedir ${OUTPUT_LOCATION} ${ORA_HOST_FLAG} ${ORA_USER_FLAG}  ${ORA_PWD_FLAG}
     else
         echo "INFO: executing: '$@'"
         exec "$@"
